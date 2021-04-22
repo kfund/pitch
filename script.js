e3f033087e59a89e3c2575b1cec15b0a8ff19e98
thanos-inevitable <69889726+thanos-inevitable@users.noreply.github.com>
2021-04-22 13:12:04 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,39 @@
+function modal (date) {
+    let dateStamp = 'null'
+    if (date == 1) {
+        dateStamp = '23.04.2021'
+    }
+    if (date == 2) {
+        dateStamp = '24.04.2021'
+    }
+    if (date == 3) {
+        dateStamp = '25.04.2021'
+    }
+    const modal = document.createElement('div')
+        modal.classList.add('modal')
+        modal.tabIndex = '-1'
+        modal.insertAdjacentHTML('afterbegin', `
+        <div class="modal-dialog">
+            <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title">Вітаю!</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <p>Результати будуть доступні: <h3>18.00 ${dateStamp}</h3></p>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-close-bottom btn-secondary" data-bs-dismiss="modal">Close</button>
+            </div>
+            </div>
+        </div>
+        `
+        )
+        document.body.appendChild(modal)
+    setTimeout(() => {
+        modal.remove()
+    }, 3000)
+    document.querySelector('.btn-close-bottom').addEventListener('click', () => {
+        modal.remove()
+    })
+}
